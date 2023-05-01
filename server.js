8ea979eced913f72657964a0e2f50a1b0dd3e33f
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-05-01 17:55:41 +0900

    s
    s

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/source', express.static('/home/ubuntu/source'))
 
 app.get("/content", async (req, res) => {
     console.log(req.query.id);
-    const count =  await DB.query("SELECT COUNT(*) as cnt FROM content WHERE category=? ",[req.query.id]);
+    const count =  await DB.query("SELECT COUNT(*) FROM content WHERE category=? ",[req.query.id]);
     const [rows, fields] = await DB.query("SELECT category, img_url, name,author, value, creator, created_at FROM content WHERE category=? ",[req.query.id]);
     const [rows2,fields2] = await DB.query("SELECT  link,description,category,name,title,img_url,creator,created_at,unit FROM category WHERE category=? ",[req.query.id]);
     
