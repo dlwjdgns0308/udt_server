dc215274d4c5d369bd8cc93d95fb14a4418b465d
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-05-08 18:27:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/2/edit_content", async (req, res) => {
 const upload = multer({ });
 app.post('/2/edit_content', upload.array('image'), (req, res) => {
   try {
-    console.log(req.body,filename[1]);
+    console.log(req.body);
     console.log(req.files)
 
     // const category = req.body.category;
