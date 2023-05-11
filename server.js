952275b095af46776b32dfc1aefb5b495612c676
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-05-11 19:02:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get("/2/edit_content", async (req, res) => {
 });
 
 app.post("/2/cancel_content", (req, res) => {
-  console.log(req.body.img_url);
+  console.log(req.body);
 });
 const upload = multer({ });
 app.post('/2/edit_content', upload.array('image'), async (req, res) => {
