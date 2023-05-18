92399dddf5e6f81e3de6ba2828762dd80b8228b8
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-05-18 22:14:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -40,11 +40,16 @@ const datetime = date.toISOString().slice(0, 19).replace('T', ' ');
 
 
 app.post("/1/edit_content/start", async (req, res) => {
-  console.log(req.body)
+  console.log(req.body.user.email)
+  const user = req.body.user.email;
+  const category = req.body.category;
   
   const [rows2,fields2] = await DB.query("SELECT  link,description,category,name,title,img_url,creator,created_at,unit,likecount,message,level FROM category WHERE category=? ",[category]);
   const [rows, fields] = await DB.query("SELECT category, img_url, name, author, value FROM content WHERE category=?", [category]);
-  if(rows2.creator == user){
+  if(rows2.creator == undefined){
+    res.status(200).send();
+    
+  }else if(rows2.creator == user){
     res.send({content:rows,title:rows2});
   }else{
     res.status(123).send('다른 유저의 접근입니다.');
