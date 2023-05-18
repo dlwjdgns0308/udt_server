936ac3b7387fb6ddf61f24308cef78bd7dc734bc
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-05-18 22:49:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,21 @@ const datetime = date.toISOString().slice(0, 19).replace('T', ' ');
 
 app.post("/1/edit_content/start", async (req, res) => {
   console.log(req.body)
-
+  const user = req.body.session.user.email;
+  const category = req.body.category;
+  
+  const [rows2,fields2] = await DB.query("SELECT  link,description,category,name,title,img_url,creator,created_at,unit,likecount,message,level FROM category WHERE category=? ",[category]);
+  const [rows, fields] = await DB.query("SELECT category, img_url, name, author, value FROM content WHERE category=?", [category]);
+  if(rows2.creator == undefined){
+    //새로운 컨텐츠
+    res.status(200).send();
+  }else if(rows2.creator == user){
+    //기존유저
+    res.status(300).send({content:rows,title:rows2});
+  }else{
+    //다른유저
+    res.status(123).send('다른 유저의 접근입니다.');
+  }
  
 });
 
