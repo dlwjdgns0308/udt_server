bb1b1e8e89a55684c6e8bd7a3aba9b6da694afdc
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-05-23 11:56:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,22 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post("/list/main",async (req, res) => {
+    console.log(req.body)
     const [rows,fields] = await DB.query("SELECT  link,description,category,name,title,img_url,creator,created_at,unit,likecount FROM category");
     
     res.send(rows);
 });
+
+// app.get("/list/main", async (req, res) => {
+//   console.log(req.query.id);
+//   const [rows,fields] = await DB.query("SELECT  user_id, user_login,user_name,profile_image_url, view_count, created_at FROM twitch_user_list WHERE user_id = ? ORDER BY view_count DESC Limit 1",[req.query.id]);
+//   const [rows2,fields2] = await DB.query("SELECT  user_id,user_name,created_at,creator_id,creator_name,run_time,video_url,thumbnail,title,view_count,game_id FROM t_clips WHERE user_id = ? ORDER BY view_count DESC Limit 5",[req.query.id]);   
+//   const [rows3,fields3] = await DB.query("SELECT  user_id,user_login,user_name,title,created_at,url,thumbnail,view_count,run_time FROM t_videos WHERE user_id = ? ORDER BY created_at desc Limit 10",[req.query.id]);   
+  
+//   console.log(rows);
+  
+//   res.send({user:rows,clip:rows2,video:rows3});
+// });
 
 
 app.post("/list/mypage",async (req, res) => {
@@ -38,10 +50,12 @@ app.post("/list/mypagedel",async (req, res) => {
   if (fs.existsSync(dir)) {
     fs.rmdirSync(dir, { recursive: true });
   }
-  res.status(200).send('ì„±ê³µì ìœ¼ë¡œ ë°ì´í„°ë¥¼ ì‚­ì œí•˜ì˜€ìŠµë‹ˆë‹¤');
+  res.status(200).send('?„±ê³µì ?œ¼ë¡? ?°?´?„°ë¥? ?‚­? œ?•˜????Šµ?‹ˆ?‹¤');
 });
 
 app.use('/source', express.static('/home/ubuntu/source'))
+
+
 
 app.get("/content", async (req, res) => {
     console.log(req.query.id);
@@ -76,13 +90,13 @@ app.post("/1/edit_content/start", async (req, res) => {
   }
   console.log(user, category,  creator)
   if( creator == undefined){
-    //ìƒˆë¡œìš´ ì»¨í…ì¸ 
+    //?ƒˆë¡œìš´ ì»¨í…ì¸?
     res.status(200).send();
   }else if( creator == user){
-    //ê¸°ì¡´ìœ ì €
+    //ê¸°ì¡´?œ ???
     res.status(201).send({content:rows,title:rows2});
   }else{
-    //ë‹¤ë¥¸ìœ ì €
+    //?‹¤ë¥¸ìœ ???
     console.log("fsfsdfdsfds")
     res.status(300).send();
   }
@@ -100,12 +114,12 @@ app.post('/1/edit_content', uploads.array('images'), async (req, res) => {
     const dir = `/home/ubuntu/source/${category}`;
     console.log(dir,category)
     
-    // ï¿½ë¤ƒï¿½ëœ‘åª›ï¿½ è­°ëŒì˜±ï¿½ë¸¯ï§ï¿½ ï¿½ë¸¡ï¿½ì‘ï§ï¿½ ï¿½ë¤ƒï¿½ëœ‘ ï¿½ê¹®ï¿½ê½¦
+    // ï¿½ë¤ƒï¿½ëœ‘åª›ï¿½ è­°ëŒ?˜±ï¿½ë¸¯ï§ï¿½ ï¿½ë¸¡ï¿½ì‘ï§ï¿½ ï¿½ë¤ƒï¿½ëœ‘ ï¿½ê¹®ï¿½ê½¦
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
     
-    // ï¿½ë™†ï¿½ì”ª ï¿½ï¿½ï¿½ï¿½ì˜£
+    // ï¿½ë™†ï¿½ì”ª ï¿½ï¿½ï¿½ï¿½?˜£
     for (let i = 0; i < req.files.length; i++) {
       const file = req.files[i];
       const filename = file.originalname;
@@ -120,15 +134,15 @@ app.post('/1/edit_content', uploads.array('images'), async (req, res) => {
     const Path2 = 'http://43.201.68.150:3001/source/'
     const filePath = `${Path2}/${category}/${filename}`;
     const message = 
-    "ì˜ëª» í´ë¦­í•˜ì‹ ê±°ì£ ? ë‹¤ì‹œ í•œ ë²ˆ ë„ì „í•´ë³´ì„¸ìš”!,ì´ë²ˆ ë ˆë²¨ì€ ì¢€ ê¹Œë‹¤ë¡œì› ë‚˜ë´ìš”. í•˜ì§€ë§Œ ë‹¤ìŒì—ëŠ” ë” ì¬ë°ŒëŠ” ë„ì „ì´ ì¤€ë¹„ë˜ì–´ ìˆì„ ê±°ì—ìš”.,ì˜¤ëŠ˜ì€ ìš´ì´ ë³„ë¡œ ì—†ì—ˆë‚˜ë´ìš”. ë‹¤ìŒì—ëŠ” ì¢€ ë” ìš´ì´ ì¢‹ê¸°ë¥¼ ë¹Œì–´ë“œë ¤ìš”!,ì´ˆë³´ìê°€ ì•„ë‹ˆì‹œêµ°ìš”! ë” ì–´ë ¤ìš´ ëª©í‘œë¥¼ í–¥í•´ ë‚˜ì•„ê°€ë³´ì„¸ìš”.,ì´ë²ˆì—ë„ ë©‹ì§„ ê²°ê³¼ë¥¼ ì´ë£¨ì…¨ìŠµë‹ˆë‹¤. í•˜ì§€ë§Œ ì´ì œë¶€í„°ëŠ” ë” í° ë„ì „ì´ ê¸°ë‹¤ë¦¬ê³  ìˆë‹µë‹ˆë‹¤.,ì´ë¯¸ ê²½í—˜ì´ ë§ìœ¼ì‹  ë¶„ì´ì‹œë‹ˆ ì´ì   ë”ìš± ë” ëŒ€ë‹¨í•œ ê²°ê³¼ë¥¼ ì´ë£¨ì…”ë„ ë©ë‹ˆë‹¤. ìš°ë¦¬ê°€ ê¸°ëŒ€í• ê²Œìš”!,ì´ë²ˆ ì‹¤íŒ¨ëŠ” ë‹¤ìŒì—ëŠ” ê¼­ ì„±ê³µí•  ìì‹ ì„ í‚¤ì›Œì¤„ ê±°ì—ìš”. ì¡°ê¸ˆë§Œ ë” ë…¸ë ¥í•˜ë©´ ë©ë‹ˆë‹¤!,ìˆ™ë ¨ìê¸‰ì´ì‹œêµ°ìš”. ì´ì   ë” ì–´ë ¤ìš´ ë„ì „ë„ ì „í˜€ ë¬´ì„­ì§€ ì•Šê² ì£ ?,ì´ë¯¸ ìµœê³ ì— ë„ë‹¬í•˜ì…¨ìŠµë‹ˆë‹¤! ì´ì œëŠ” ë” ììœ ë¡­ê²Œ ë„ì „í•´ë³´ì„¸ìš”. ë‹¹ì‹ ì˜ ì¬ëŠ¥ì„ ë³´ì—¬ì£¼ì„¸ìš”!,ì´ë²ˆ ê²°ê³¼ëŠ” ì—­ëŒ€ê¸‰ì…ë‹ˆë‹¤! ë‹¹ì‹ ì´ ì´ ê²Œì„ì˜ ì „ì„¤ì´ ë  ê±°ì—ìš”.";
-    const level = "ì´ˆë³´ì,í•™ìŠµì,ìˆ˜ë ¨ìƒ,ì „ë¬¸ê°€,ë² í…Œë‘,ìŠ¤í˜ì…œë¦¬ìŠ¤íŠ¸,ê³ ìˆ˜,ë§ˆìŠ¤í„°,ê±°ì¥,ëŒ€ê°€,ì „ì„¤";
+    "?˜ëª? ?´ë¦??•˜?‹ ê±°ì£ ? ?‹¤?‹œ ?•œ ë²? ?„? „?•´ë³´ì„¸?š”!,?´ë²? ? ˆë²¨ì?? ì¢? ê¹Œë‹¤ë¡œì› ?‚˜ë´ìš”. ?•˜ì§?ë§? ?‹¤?Œ?—?Š” ?” ?¬ë°ŒëŠ” ?„? „?´ ì¤?ë¹„ë˜?–´ ?ˆ?„ ê±°ì—?š”.,?˜¤?Š˜??? ?š´?´ ë³„ë¡œ ?—†?—ˆ?‚˜ë´ìš”. ?‹¤?Œ?—?Š” ì¢? ?” ?š´?´ ì¢‹ê¸°ë¥? ë¹Œì–´?“œ? ¤?š”!,ì´ˆë³´?ê°? ?•„?‹ˆ?‹œêµ°ìš”! ?” ?–´? ¤?š´ ëª©í‘œë¥? ?–¥?•´ ?‚˜?•„ê°?ë³´ì„¸?š”.,?´ë²ˆì—?„ ë©‹ì§„ ê²°ê³¼ë¥? ?´ë£¨ì…¨?Šµ?‹ˆ?‹¤. ?•˜ì§?ë§? ?´? œë¶??„°?Š” ?” ?° ?„? „?´ ê¸°ë‹¤ë¦¬ê³  ?ˆ?‹µ?‹ˆ?‹¤.,?´ë¯? ê²½í—˜?´ ë§ìœ¼?‹  ë¶„ì´?‹œ?‹ˆ ?´?   ?”?š± ?” ????‹¨?•œ ê²°ê³¼ë¥? ?´ë£¨ì…”?„ ?©?‹ˆ?‹¤. ?š°ë¦¬ê?? ê¸°ë???• ê²Œìš”!,?´ë²? ?‹¤?Œ¨?Š” ?‹¤?Œ?—?Š” ê¼? ?„±ê³µí•  ??‹ ?„ ?‚¤?›Œì¤? ê±°ì—?š”. ì¡°ê¸ˆë§? ?” ?…¸? ¥?•˜ë©? ?©?‹ˆ?‹¤!,?ˆ™? ¨?ê¸‰ì´?‹œêµ°ìš”. ?´?   ?” ?–´? ¤?š´ ?„? „?„ ? „??? ë¬´ì„­ì§? ?•Šê² ì£ ?,?´ë¯? ìµœê³ ?— ?„?‹¬?•˜?…¨?Šµ?‹ˆ?‹¤! ?´? œ?Š” ?” ??œ ë¡?ê²? ?„? „?•´ë³´ì„¸?š”. ?‹¹?‹ ?˜ ?¬?Š¥?„ ë³´ì—¬ì£¼ì„¸?š”!,?´ë²? ê²°ê³¼?Š” ?—­???ê¸‰ì…?‹ˆ?‹¤! ?‹¹?‹ ?´ ?´ ê²Œì„?˜ ? „?„¤?´ ?  ê±°ì—?š”.";
+    const level = "ì´ˆë³´?,?•™?Šµ?,?ˆ˜? ¨?ƒ,? „ë¬¸ê??,ë² í…Œ?‘,?Š¤?˜?…œë¦¬ìŠ¤?Š¸,ê³ ìˆ˜,ë§ˆìŠ¤?„°,ê±°ì¥,???ê°?,? „?„¤";
     // DBï¿½ë¿‰ ï¿½ëœ²ï¿½ì” ï¿½ê½£ ï¿½ê¶«ï¿½ì—¯
     const sql = "INSERT INTO category (link, description, category, name, title, img_url, creator, created_at, unit, likecount,message,level) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    const values = [`./content/${category}`, description, category,  file.originalname, title, filePath , user,datetime, 'ì›', 0,message, level];
+    const values = [`./content/${category}`, description, category,  file.originalname, title, filePath , user,datetime, '?›', 0,message, level];
     const [rows, fields] = await DB.query(sql, values);
 
     console.log(rows);
-    res.send('ì„±ê³µ');
+    res.send('?„±ê³?');
   }catch(error){
     res.status(405).send(error);
   }
@@ -140,12 +154,12 @@ app.post('/1/edit_content', uploads.array('images'), async (req, res) => {
   const dir = `/home/ubuntu/source/${category}`;
   console.log(dir,category)
   
-  // ï¿½ë¤ƒï¿½ëœ‘åª›ï¿½ è­°ëŒì˜±ï¿½ë¸¯ï§ï¿½ ï¿½ë¸¡ï¿½ì‘ï§ï¿½ ï¿½ë¤ƒï¿½ëœ‘ ï¿½ê¹®ï¿½ê½¦
+  // ï¿½ë¤ƒï¿½ëœ‘åª›ï¿½ è­°ëŒ?˜±ï¿½ë¸¯ï§ï¿½ ï¿½ë¸¡ï¿½ì‘ï§ï¿½ ï¿½ë¤ƒï¿½ëœ‘ ï¿½ê¹®ï¿½ê½¦
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
   
-  // ï¿½ë™†ï¿½ì”ª ï¿½ï¿½ï¿½ï¿½ì˜£
+  // ï¿½ë™†ï¿½ì”ª ï¿½ï¿½ï¿½ï¿½?˜£
   for (let i = 0; i < req.files.length; i++) {
     const file = req.files[i];
     const filename = file.originalname;
@@ -161,11 +175,11 @@ app.post('/1/edit_content', uploads.array('images'), async (req, res) => {
   const filePath = `${Path2}/${category}/${filename}`;
   // DBï¿½ë¿‰ ï¿½ëœ²ï¿½ì” ï¿½ê½£ ï¿½ê¶«ï¿½ì—¯
   const sql = "INSERT INTO category (link, description, category, name, title, img_url, creator, created_at, unit, likecount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-  const values = [`./content/${category}`, description, category,  file.originalname, title, filePath , user,datetime, 'ì›', 0];
+  const values = [`./content/${category}`, description, category,  file.originalname, title, filePath , user,datetime, '?›', 0];
   const [rows, fields] = await DB.query(sql, values);
 
   console.log(rows);
-  res.send('ì²˜ë¦¬ë˜ì—ˆìŠµë‹ˆë‹¤');
+  res.send('ì²˜ë¦¬?˜?—ˆ?Šµ?‹ˆ?‹¤');
 });
 
   
@@ -218,7 +232,7 @@ app.post('/2/edit_content', upload.array('image'), async (req, res) => {
       // const file = req.files[i];
       // const name = req.body.name[i];
       // const filePath = `${dir}/${name}`;
-      // // ï¿½ë‹”ï¿½ì ™è‘ì‡°â” ï¿½ê½”æ¹²ï¿½
+      // // ï¿½ë‹”ï¿½ì ™?‘?‡°?” ï¿½ê½”æ¹²ï¿½
       const sql = 'UPDATE content SET name = ?, age = ? WHERE id = ?;';
       // const sql = "INSERT INTO content (category, img_url, name,author, value) VALUES (?, ?, ?, ?, ?) ";
       // const values = [`${category}`, `http://43.201.68.150:3001/source/${category}/${filename}`, filename, null,"0"];
