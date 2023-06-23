31c7fbc8b0b0f2e676d5dabf0955bd0d02b0c9e3
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-06-23 21:40:28 +0900

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host : 'svc.sel4.cloudtype.app',
     port: 31405,
-    user : 'user',
+    user : 'root',
     password : '0308',
     database : 'udtown'
 });
