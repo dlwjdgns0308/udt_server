93f82c81a81221d1023f2c2f5246d3a03accd4b6
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-06-29 11:40:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
 const { error } = require("console");
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
 const multerS3 = require('multer-s3');
 require('dotenv').config();
 
