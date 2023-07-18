0c997d355647beda779b4d8791375f5c4caf1206
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-07-18 20:43:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -345,8 +345,6 @@ app.post('/1/edit_content', uploads.array('images'), async (req, res) => {
             await s3.upload(uploadParams).promise();
               // MySQL에 이미지 URL 저장
             const sql = "INSERT INTO content (category, img_url, name,author, value) VALUES (?, ?, ?, ?, ?) ";
-            const real = filename.replace(".jpg","");
-            real = filename.replace(".png","");
             const values = [`${category}`, imageUrl, real, null,"0"];
             const [rows, fields] = await DB.query(sql, values);
             console.log(`${filename} 업로드 완료`);
